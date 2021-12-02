e0195dac9991f482bddeb186df7c7645b83a250b
ParkJong-Hun <thesn515@gmail.com>
2021-12-02 21:59:54 +0900

    2021/12/02 21:59 Navigation

---
+++ chapter9/navigation/app/src/main/java/com/parkjonghun/navigation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.parkjonghun.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    //Appbar(ActionBar)の情報があるオブジェクト
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //ActionBarをNavigation専用（Backボタンが出される）に変える
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    //Backボタンを押したら以前の画面に戻れるように
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
---
+++ chapter9/navigation/app/src/main/java/com/parkjonghun/navigation/MainFragment.kt
@@ -0,0 +1,60 @@
+package com.parkjonghun.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.parkjonghun.navigation.databinding.FragmentMainBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class MainFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    //ViewBinding
+    private lateinit var binding:FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //FragmentではここでBindingを取る
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener{
+            //移動する画面と共に送るArgument（情報など）を含めてるAction
+            val action = MainFragmentDirections.actionMainFragmentToSecondFragment2("Argumentを送る!!!")
+            //他の画面に移動
+            //findNavController().navigate(R.id.action_mainFragment_to_secondFragment2)
+            findNavController().navigate(action)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
